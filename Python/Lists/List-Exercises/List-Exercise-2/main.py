1e82c19fa969bf21aa133d2b3e838451e3488cdc
Elliot Tam <eteaisme@gmail.com>
2024-09-27 20:39:28 -0400

    Added examples for exercise 2

--- Python/Lists/List-Exercises/List-Exercise-2/main.py
+++ Python/Lists/List-Exercises/List-Exercise-2/main.py
@@ -1,7 +1,9 @@
 def is_descending(array):
-  if len(array) <= 1:
+  if len(array) == 1:
     return True
   if(array[0] < array[1]):
     return False
   return is_descending(array[1:])
 
+print(is_descending([20,19,16,9,3,1,0])) 
+print(is_descending([36,29,57,16,12,4,3])) 
