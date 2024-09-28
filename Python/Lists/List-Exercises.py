b038cbf77242ee308272bbb34b2a3ab62be9f5ef
Elliot Tam <eteaisme@gmail.com>
2024-09-27 20:15:03 -0400

    Added exercise dirrectories

--- Python/Lists/List-Exercises.py
+++
@@ -1,8 +0,0 @@
-my_list = [9, 2, 4 ,5, 1, -2, 4]
-my_list.append(10)
-
-running_total = 0
-for i in range(len(my_list)):
-    running_total = running_total + my_list[i]
-
-print("The sum of this list is: ", running_total)
