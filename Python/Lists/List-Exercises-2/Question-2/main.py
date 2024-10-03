7248cb1f9557eef402f81abee725689f838ff32b
Elliot <eteaisme@gmail.com>
2024-10-03 10:10:53 -0400

    tic toe toe

--- Python/Lists/List-Exercises-2/Question-2/main.py
+++ Python/Lists/List-Exercises-2/Question-2/main.py
@@ -1,11 +1,2 @@
 def lockerProblem(numberOfLockers):
-    currentStudentLockers = []
-    totalStudentLockers = []
-    for i in range (int(numberOfLockers)):
-        currentStudentLockers.append(i)
-        totalStudentLockers += currentStudentLockers
-
-    return totalStudentLockers
     
-
-print(lockerProblem(1000))
