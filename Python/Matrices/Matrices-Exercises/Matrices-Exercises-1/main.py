45933abc49dd26b65e36b177e2ee66854ebef607
Elliot <eteaisme@gmail.com>
2024-10-03 09:42:09 -0400

    Added solution for exercise 1

--- Python/Matrices/Matrices-Exercises/Matrices-Exercises-1/main.py
+++ Python/Matrices/Matrices-Exercises/Matrices-Exercises-1/main.py
@@ -1,24 +1,7 @@
-canadianFRCTeams = ["865", "610", "2056"]
-americanFRCTeams = ["254", "2910", "4414"]
-
-FRCTeams = [canadianFRCTeams, americanFRCTeams]
-
-for i in range(len(FRCTeams)):
-    for j in range(len(FRCTeams[i])):
-        print(FRCTeams[i][j], end = " ")
-    print("")
-
-
-
-print("")
-timesTable = [([0] * 12) for i in range(12)]
-
-for i in range (12):
-    for j in range (12):
-        timesTable[i][j] = ((i+1)*(j+1))
-        
-
-for i in range(len(timesTable)):
-    for j in range(len(timesTable[i])):
-        print(timesTable[i][j], end = " ")
-    print("")
+def matrix_print(matrix):
+    for i in range(len(matrix)):
+        for j in range(len(matrix[i])):
+            print(matrix[i][j], end = " ")
+        print("")
+myMatrix = [[1,2,3,4,5,6],[7,8,9,10,11,12]]
+matrix_print(myMatrix)
