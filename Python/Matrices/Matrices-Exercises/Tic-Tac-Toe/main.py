d683eea80cf7a99ea7fbeaba48e789754919600e
Elliot <eteaisme@gmail.com>
2024-10-04 09:16:40 -0400

    Hey its working now

--- Python/Matrices/Matrices-Exercises/Tic-Tac-Toe/main.py
+++ Python/Matrices/Matrices-Exercises/Tic-Tac-Toe/main.py
@@ -1,18 +1,32 @@
-gameState = [("_")*3 for i in range(3)]
+#Last Updated: 03/10/2024
 
-def printGameState(matrix):
-    for i in range(len(matrix)):
-        for j in range(len(matrix[i])):
-            print(matrix[i][j], end = " ")
-        print("")
-def getInput(gameState):
-    row, collum = map(int, input("Where do you want to move: ").split())
-    print("Row: ", row)
-    print("Collum: ", collum)
-    choice = input("What move do you want ot make: ")
-    gameState[[row][collum]] = choice
-    print(gameState[0][])
+class Player:
+    def __init__(self, symbol):
+        self.symbol = symbol
+        self.winnner = False
+        self.board = [' ' for _ in range(9)]
     
-printGameState(gameState)
-getInput(gameState)
-printGameState(gameState)
+
+    def checkWinner(self, board):
+        winConditions = [(0,1,2), (3,4,5), (6,7,8), # Rows
+                            (0,3,6), (1,4,7), (2,5,8), # Columns
+                            (0,4,8), (2,4,6)] # Diagonals
+        for condition in winConditions:
+            if board[condition[0]]== self.symbol:
+                self.winnner = True
+
+    def printBoard(self):
+        print("\n")
+        for i in range(0, 9, 3):
+            print(f" {self.board[i]} | {self.board[i+1]} | {self.board[i+2]} ")
+            if i < 6:
+                print("---|---|---")
+        print("\n")    
+
+humanPlayer = Player("X")
+humanPlayer.board[0] = humanPlayer.symbol
+humanPlayer.board[1] = humanPlayer.symbol
+humanPlayer.board[2] = humanPlayer.symbol
+humanPlayer.printBoard()
+humanPlayer.checkWinner(humanPlayer.board)
+print(humanPlayer.winnner)
