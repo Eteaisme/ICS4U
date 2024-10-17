33d02059b82bec57e5c79081d05a7531d105d5e8
Elliot <eteaisme@gmail.com>
2024-10-17 09:08:41 -0400

    Refactored varible names

--- Python/OOP/Pair_Programing/main.py
+++ Python/OOP/Pair_Programing/main.py
@@ -3,20 +3,20 @@ class Point:
     def __init__(self, x=0, y = 0):
 
     # getters:  get the x and y coordinates, returns a float
-    def get_x(self):
+    def getX(self):
         
-    def get_y(self):
+    def getY(self):
 
     # setters set the x and y coordinates
-    def set_x (self, x):
+    def setX (self, x):
         
-    def set_y(self, y):
+    def setY(self, y):
 
     # distance to another point, p, returns a float
-    def dist_to_point(self, p):
+    def distanceToPoint(self, point):
 
     #distance to origin, returns a float
-    def dist_to_origin(self):
+    def distanceToOrigin(self):
 
     # return string representation of a point, i.e. coordinate pair (x,y)
     def __repr__(self):
@@ -31,23 +31,23 @@ class Line:
     def __init__ (slope = 1, yint =0):
 
     # getters, return floats
-    def get_slope (self):
+    def getSlope (self):
         
-    def get_yint(self):
+    def getYIntercept(self):
 
     # setters
-    def set_slope (self, slope):
+    def setSlope (self, slope):
         
-    def set_yint (self, yint):
+    def setYIntercept (self, yint):
 
     # determine if two lines are parallel, return boolean
-    def is_parallel (self,other):
+    def isParrallel (self,other):
 
     # determine if two lines are perpendicular to each other, return boolean
-    def is_perpendicular (self, other):
+    def isPerpendicular(self, other):
 
     # determine if a point, p, is on the line return boolean
-    def on_line(self, p):
+    def onLine(self, p):
 
     # return string representation of the slope and intercept of a line, e.g.
     # slope: 1.0 intercept: 0.0
@@ -59,22 +59,22 @@ class Line:
 
 ######## Test Code ####################3
 p1 = Point()
-p1.set_x(3)
-p1.set_y(4)
+p1.setX(3)
+p1.setY(4)
 print("Point 1: ", p1)
-print("Distance to origin: ", p1.dist_to_origin())
+print("Distance to origin: ", p1.distanceToOrigin())
 p2 = Point(-1,-10)
-print("p2 is at (", p2.get_x(),",",p2.get_y(),")")
-print("Distance p1 to p2: ", p1.dist_to_point(p2))
+print("p2 is at (", p2.getX(),",",p2.getY(),")")
+print("Distance p1 to p2: ", p1.distanceToPoint(p2))
 print("Does p1=p2? ", p1==p2)
 
 l1=Line()
-l1.set_slope(-2)
-l1.set_yint(10)
+l1.setSetSlope(-2)
+l1.setYIntercept(10)
 print(l1)
 l2=Line(0.5,-3)
 print("Does l1 =l2?: ", l1==l2)
-print("Are l1 and l2 parallel? ", l1.is_parallel(l2))
-print("Are l1 and l2 perpendicular? ", l2.is_perpendicular(l1))
-print("Is p1 on l1? ", l1.on_line(p1))
-print("Is p2 on l2? ", l2.on_line(p2))
+print("Are l1 and l2 parallel? ", l1.isParrallel(l2))
+print("Are l1 and l2 perpendicular? ", l2.isPerpendicular(l1))
+print("Is p1 on l1? ", l1.onLine(p1))
+print("Is p2 on l2? ", l2.onLine(p2))
